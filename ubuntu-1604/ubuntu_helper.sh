d5c6bbdf88798bda75afcded54a57b9fe590a9cd
Lincoln Thurlow <lincoln@isi.edu>
2018-03-29 19:39:20 -0700

    agetty configuration
    decided on keeping Type=idle to make sure the user is aware that
    the system is not "fully" booted, and that the tty may not be
    responsive.
    
    User must wait for about 2 minutes for the serial console to timeout
    before input works.

--- ubuntu-1604/ubuntu_helper.sh
+++ ubuntu-1604/ubuntu_helper.sh
@@ -132,21 +132,38 @@ systemctl set-default multi-user.target
 
 # have serial console come up on systemd - again NOTE hard coded ttyS0
 cat <<'EOF' > /etc/systemd/system/getty.target.wants/getty\@ttyS1.service
+# http://man7.org/linux/man-pages/man8/agetty.8.html
+# https://www.freedesktop.org/software/systemd/man/systemd.unit.html
 # https://ubuntuforums.org/showthread.php?t=2343595
 [Unit]
 Description=Serial Console
+
+# After= ensures that the configured unit is started after the listed unit finished
+# starting up, Before= ensures the opposite, that the configured unit is fully started
+# up before the listed unit is started.
 After=systemd-user-sessions.service plymouth-quit-wait.service
 After=rc-local.service
 Before=getty.target
+
+# Takes a boolean argument. If true, this unit will not be stopped when isolating
+# another unit. Defaults to false for service, target, socket, busname, timer, and
+# path units, and true for slice, scope, device, swap, mount, and automount units.
 IgnoreOnIsolate=yes
 
+# ConditionPathExists=/dev/tty0
+
 [Service]
-# ExecStart=-/sbin/agetty --noclear %I $TERM
-/sbin/agetty -L 115200 %I $TERM
+# the VT is cleared by TTYVTDisallocate
+ExecStart=-/sbin/agetty %I $TERM
+Type=idle
 Restart=always
+UtmpIdentifier=%I
+TTYPath=/dev/%I
+TTYReset=yes
 
 [Install]
 WantedBy=multi-user.target
+DefaultInstance=tty1
 EOF
 
 # leave chroot
